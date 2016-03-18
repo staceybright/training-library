45f6f760aa59704bd04787b89231bde6a214441e
Joseph Long <jlong@stsci.edu>
2016-03-18 16:30:58 -0400

    Fix/silence some warnings

--- source/conf.py
+++ source/conf.py
@@ -57,16 +57,16 @@ copyright = (u'{}, AURA / Space Telescope Science Institute. These documents '
 # is part of the template
 
 # fill in copyright year
-import datetime
-today = datetime.date.today()
-copyright = copyright.format(today.year)
+import datetime as _datetime
+_today = _datetime.date.today()
+copyright = copyright.format(_today.year)
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
 # The short X.Y version.
-version = 'published {}-{}-{}'.format(today.year, today.month, today.day)
+version = 'published {}-{}-{}'.format(_today.year, _today.month, _today.day)
 # The full version, including alpha/beta/rc tags.
 release = version
 
@@ -135,7 +135,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'RIA Training Library (v{})'.format(release)
+html_title = 'RIA Training Library'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
