cc3c5e2324f731dc61d8815b931c2ba6d4eb1406
James White <jwhite@stsci.edu>
2018-06-25 16:44:59 -0400

    Generated gh-pages for commit 4eb4537 Merge: 1a0ac35 3d828f9 Author: Lauren Chambers <Lauren.m.chambers96@gmail.com>
        Merge pull request #7 from spacetelescope/chambers_version-control

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'published 2018-6-14',
+    VERSION: 'published 2018-6-25',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
